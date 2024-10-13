a3618a6c76fafac84ee0584f7ee420d5f36285de
SenalAriyaratne <ariyaratne.senal@gmail.com>
2024-10-13 15:38:47 +0530

    add repr to node

--- src/doubly_linked_list/node.py
+++ src/doubly_linked_list/node.py
@@ -22,3 +22,8 @@ class Node:
     
     def get_previous(self):
         return self._prev
+    
+    def __repr__(self):
+        nxt_data = self._nxt._data if self._nxt else None
+        prev_data = self._prev._data if self._prev else None
+        return f"Node(data={self._data}, prev={prev_data}, nxt={nxt_data})"
